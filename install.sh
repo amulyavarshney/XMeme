cad2dc41f18df577ce6e322cf3ab2760830c8ec2
amulyavarshney <amulyavarshney7@gmail.com>
2021-02-20 18:06:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 
 # Any installation related commands
 
-sudo apt-get install -y python3
+sudo apt-get install -y python3.8
 sudo apt install -y python3-pip
-/usr/bin/pip3 install -r backend/requirements.txt
+# /usr/bin/pip3 install -r backend/requirements.txt
+sudo -H pip install -r backend/requirements.txt
 
 
 # Any configuration related commands
